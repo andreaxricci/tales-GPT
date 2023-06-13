165e223e1f88abfe1bb2dec11af534f388adc422
andreaxricci <62494809+andreaxricci@users.noreply.github.com>
2023-06-13 11:35:08 +0200

    passing variables as session states

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from speech import recognize_from_microphone, synthetise_speech
 from utils import generate_story, load_api_keys, language2code
 
 
-language = 'Portuguese'
-gender = 'male'
-model_name = 'gpt-3.5-turbo'
+#language = 'Portuguese'
+#gender = 'male'
+#model_name = 'gpt-3.5-turbo'
 temperature = 1
 
 load_api_keys()
@@ -17,8 +17,23 @@ openai_api_kei = os.environ.get('OPENAI_API_KEY')
 
 def main() -> None:
      
-    st.set_page_config(page_title="talesGPT", page_icon=":heart:")
+    st.set_page_config(page_title="talesGPT", page_icon=":heart:", layout='wide')
     st.subheader("Tell me a story")
+
+    with st.sidebar:
+
+        model_name = st.radio(
+        "Select the model",
+        ('gpt-3.5-turbo', 'text-davinci-003', 'gpt-4'))
+
+        gender = st.radio(
+        "Select the gender of the narrator",
+        ('female', 'male'))
+
+        language = st.radio(
+            "Select the language",
+            ('Portuguese', 'Italian', 'Swiss German'))
+
 
     if "button1" not in st.session_state:
         st.session_state["button1"] = False        
@@ -27,16 +42,16 @@ def main() -> None:
         st.session_state["button2"] = False
 
     if "button3" not in st.session_state:
-        st.session_state["button3"] = False
+        st.session_state["button3"] = False 
 
-
-    if st.button("Button1"):
+    if st.button("A story about ..."):
         # toggle button1 session state  
         st.session_state["button1"] = not st.session_state["button1"]
         try:
             with st.spinner("Recording audio..."):
                 # Trigger audio recording
                 user_input = recognize_from_microphone(language2code(language))
+                st.session_state["user_input"] = user_input
                 st.write(user_input)
     
         except Exception as e:
@@ -44,39 +59,31 @@ def main() -> None:
 
 
     if st.session_state["button1"]:
-        if st.button("Button2"):
+        if st.button("Generate Story"):
+            user_input = st.session_state["user_input"]
             print(f"user_input: {user_input}")
             # toggle button2 session state
             st.session_state["button2"] = not st.session_state["button2"]
-            
-            #story = generate_story(user_input, language, model_name, temperature, openai_api_kei)
-            story = 'hello world'
-            print(f"story: {story}")
-            st.text(story)
+
+            story = generate_story(user_input, language, model_name, temperature, openai_api_kei)
+            st.session_state["story"] = story
+
+            st.write(user_input)
+            st.write(story)
 
 
     if st.session_state["button1"] and st.session_state["button2"]:
-        if st.button("Button3"):
+        if st.button("Narrate Story"):
             # toggle button3 session state
             st.session_state["button3"] = not st.session_state["button3"]
+
+            user_input = st.session_state["user_input"]
+            story = st.session_state["story"]
             synthetise_speech(story, language, gender)
 
 
     if st.session_state["button3"]:
         st.write("End")
-
-
-    # Print the session state to make it easier to see what's happening
-    st.write(
-        f"""
-        ## Session state:
-        {st.session_state["button1"]=}
-
-        {st.session_state["button2"]=}
-
-        {st.session_state["button3"]=}
-        """
-    )
 
 
 if __name__ == '__main__':
